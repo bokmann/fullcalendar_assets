2221d48973980e68e53feef7a972be713ce93a9c
bokmann <dbock@codesherpas.com>
2012-03-21 18:08:39 -0400

    adding the filtering the calendar will need to the events index controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,14 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    # full_calendar will hit the index method with query parameters
+    # 'start' and 'end' in order to filter the results for the
+    # appropriate month/week/day.  It should be possiblt to change
+    # this to be starts_at and ends_at to match rails conventions.
+    # I'll eventually do that to make the demo a little cleaner.
+    @events = Event.scoped  
+    @events = @events.after(params['start']) if (params['start'])
+    @events = @events.before(params['end']) if (params['end'])
 
     respond_to do |format|
       format.html # index.html.erb
