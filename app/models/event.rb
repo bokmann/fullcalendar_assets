42feb86725ba1b1df27bd3431d316f303f816334
bokmann <dbock@codesherpas.com>
2012-03-21 18:02:34 -0400

    adding methods the model will need to provide to the javascript.
    This code was taken verbatim from my earlier rails3_fullcalendar
    example.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,26 @@
 class Event < ActiveRecord::Base
+  
+  scope :before, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
+  scope :after, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
+  
+  # need to override the json view to return what full_calendar is expecting.
+  # http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
+  def as_json(options = {})
+    {
+      :id => self.id,
+      :title => self.title,
+      :description => self.description || "",
+      :start => starts_at.rfc822,
+      :end => ends_at.rfc822,
+      :allDay => self.all_day,
+      :recurring => false,
+      :url => Rails.application.routes.url_helpers.event_path(id)
+    }
+    
+  end
+  
+  def self.format_date(date_time)
+    Time.at(date_time.to_i).to_formatted_s(:db)
+  end
+  
 end
