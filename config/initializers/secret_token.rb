cb940a8ec39d5552d60442f63e363e2d9c20cced
bokmann <dbock@codesherpas.com>
2012-03-21 17:44:56 -0400

    initial generation of a rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullcalendarAssets::Application.config.secret_token = '9a0f2eb3f92a6e4324fd5cabc1aea35be27947270b08d2666b0c02a556409d75e7ad3e7c0172c9dea683a1b33201cb4a4e020471e1f06e627215aa6999ed86ca'
