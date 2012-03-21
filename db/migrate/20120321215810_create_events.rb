1c80ff8439e79dba246ac6e3d07dccb0c7c5b534
bokmann <dbock@codesherpas.com>
2012-03-21 18:00:35 -0400

    adding an event resource.
    These files were generated with the scafold command:
    
    rails g scaffold event title:string starts_at:datetime ends_at:datetime
    all_day:boolean description:text
    
    I didn't check in the helper or the helper test though. They would be
    empty in this app and distracting.

---
+++ db/migrate/20120321215810_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.datetime :starts_at
+      t.datetime :ends_at
+      t.boolean :all_day
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
