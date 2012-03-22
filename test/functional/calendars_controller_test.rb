bd504cc9a4e9bdea05939f00b269a70fbcc97d52
bokmann <dbock@codesherpas.com>
2012-03-21 21:03:59 -0400

    restoring a test that broken when I renamed the singular controller

--- test/functional/calendars_controller_test.rb
+++ test/functional/calendars_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CalendarControllerTest < ActionController::TestCase
+class CalendarsControllerTest < ActionController::TestCase
   test "should get show" do
     get :show
     assert_response :success
