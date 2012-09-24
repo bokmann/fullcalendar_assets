f5d98055a2523f21d6be64d888f97bb35eea25d7
bokmann <dbock@codesherpas.com>
2012-09-24 12:28:03 -0400

    resolving an issue with dates that cross calendar boundaries

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    # full_calendar will hit the index method with query parameters
-    # 'start' and 'end' in order to filter the results for the
-    # appropriate month/week/day.  It should be possiblt to change
-    # this to be starts_at and ends_at to match rails conventions.
-    # I'll eventually do that to make the demo a little cleaner.
-    @events = Event.scoped  
-    @events = @events.after(params['start']) if (params['start'])
-    @events = @events.before(params['end']) if (params['end'])
-
+    @events = Event.scoped
+    @events = Event.between(params['start'], params['end']) if (params['start'] && params['end'])
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @events }
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
-  
-  scope :before, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
-  scope :after, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
-  
+  scope :between, lambda {|start_time, end_time|
+    {:conditions => ["? < starts_at < ?", Event.format_date(start_time), Event.format_date(end_time)] }
+  }
+
   # need to override the json view to return what full_calendar is expecting.
   # http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
   def as_json(options = {})
@@ -17,11 +17,11 @@ class Event < ActiveRecord::Base
       :url => Rails.application.routes.url_helpers.event_path(id),
       #:color => "red"
     }
-    
+
   end
-  
+
   def self.format_date(date_time)
     Time.at(date_time.to_i).to_formatted_s(:db)
   end
-  
+
 end
